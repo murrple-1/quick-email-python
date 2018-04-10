b5f4fd6be10e49f36c68b3192f78d82a816640ee
Murray Christopherson <murraychristopherson@gmail.com>
2018-04-09 20:04:12 -0400

    Adding actual test as a first step

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -2,8 +2,12 @@ import unittest
 
 from quick_email import builder
 
+
 class TestBuilder(unittest.TestCase):
-    pass
+    # TODO write test suite
+    def test_build_msg(self):
+        msg = builder.build_msg('Test <test@test.com>', 'Example <example@example.com>', None, None, 'The Subject', 'Some Text', '<b>Some Bold Text</b>', attachment_list=None, inline_attachment_dict=None)
+        self.assertIsNotNone(msg)
 
 if __name__ == '__main__':
     unittest.main()
