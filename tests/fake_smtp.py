d73d6e3afa29477fdfc52792b56161ce95e7c76d
Murray Christopherson <murraychristopherson@gmail.com>
2018-04-10 11:31:54 -0400

    Removing reliance on actual SMTP and Post-Script

---
+++ tests/fake_smtp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+import smtpd
+import asyncore
+
+SMTP_PORT = int(os.environ.get(u'SMTP_PORT', u'25'))
+
+
+class FakeSMTPServer(smtpd.SMTPServer):
+    def __init__(*args, **kwargs):
+        smtpd.SMTPServer.__init__(*args, **kwargs)
+
+    def process_message(*args, **kwargs):
+        pass
+
+if __name__ == '__main__':
+    smtp_server = FakeSMTPServer(('localhost', SMTP_PORT), None)
+    try:
+        asyncore.loop()
+    except KeyboardInterrupt:
+        smtp_server.close()
--- tests/test_sender.py
+++ tests/test_sender.py
@@ -1,40 +1,19 @@
 import unittest
 import os
-import time
 
 from quick_email import builder, sender
 
-from tests.postshift import PostShift
-
-SMTP_HOST = os.environ[u'SMTP_HOST']
-SMTP_PORT = int(os.environ[u'SMTP_PORT'])
-SMTP_USER = os.environ[u'SMTP_USER']
-SMTP_PASSWORD = os.environ[u'SMTP_PASSWORD']
+SMTP_HOST = os.environ.get(u'SMTP_HOST', u'localhost')
+SMTP_PORT = int(os.environ.get(u'SMTP_PORT', u'25'))
+SMTP_USER = os.environ.get(u'SMTP_USER', u'testuser')
+SMTP_PASSWORD = os.environ.get(u'SMTP_PASSWORD', u'password')
 SMTP_IS_TLS = os.environ.get(u'SMTP_IS_TLS') == u'true'
-SMTP_SENDER = os.environ[u'SMTP_SENDER']
 
 
 class TestSender(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.postshift = PostShift()
-
-        for i in range(6):
-            cls.postshift.create()
-
     def test_send_msg(self):
-        test_email_json = TestSender.postshift.email_jsons[0]
-
-        msg = builder.build_msg(SMTP_SENDER, test_email_json[u'email'], None, None, u'The Subject', u'Some Text', u'<b>Some Bold Text</b>', attachment_list=None, inline_attachment_dict=None)
-        sender.send_msg(SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD, SMTP_IS_TLS, SMTP_SENDER, msg)
-
-        # wait for email to arrive
-        time.sleep(30.0)
-
-        _json = TestSender.postshift.get_list(test_email_json[u'key'])
-
-        self.assertIsInstance(_json, list)
-        self.assertEqual(len(_json), 1)
+        msg = builder.build_msg(u'Example <example@example.com>', u'Test <test@test.com>', None, None, u'The Subject', u'Some Text', u'<b>Some Bold Text</b>', attachment_list=None, inline_attachment_dict=None)
+        sender.send_msg(SMTP_HOST, SMTP_PORT, SMTP_USER, SMTP_PASSWORD, SMTP_IS_TLS, u'Example <example@example.com>', msg)
 
 
 if __name__ == u'__main__':
