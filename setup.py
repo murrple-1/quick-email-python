f4bdbb2bcbf16e2a3bd1186f1b0ed3093728b650
Murray Christopherson <murraychristopherson@gmail.com>
2018-04-11 09:33:20 -0400

    Upping version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='quick-email',
-    version='0.1.0',
+    version='0.2.0',
     description='Quick-and-Dirty Email Sender',
     long_description=long_description,
     url='https://github.com/murrple-1/send-email-python',
