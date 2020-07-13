f593bf79d0eef25304b55db72e2bf93895efcb98
Murray Christopherson <murraychristopherson@gmail.com>
2020-07-13 12:29:35 -0400

    improving the tests

--- tests/test_quick_email.py
+++ tests/test_quick_email.py
@@ -6,15 +6,20 @@ import tests.smtp as smtp
 
 
 class TestQuickEmail(unittest.TestCase):
+    def run(self, *args, **kwargs):
+        result = super().run(*args, **kwargs)
+
+        time.sleep(smtp.SMTP_RATE_LIMIT_SECONDS)
+
+        return result
+
     def test_send_email_auth(self):
         send_email(smtp.SMTP_HOST, smtp.SMTP_PORT, u'Example <example@example.com>', u'The Subject', send_to=u'Test <test@test.com>',
                    plain_text=u'Some Text', html_text=u'<b>Some Bold Text</b>', username=smtp.SMTP_USERNAME, password=smtp.SMTP_PASSWORD)
-        time.sleep(smtp.SMTP_RATE_LIMIT_SECONDS)
 
     def test_send_email_starttls_auth(self):
         send_email(smtp.SMTP_HOST, smtp.SMTP_PORT, u'Example <example@example.com>', u'The Subject', send_to=u'Test <test@test.com>',
                    plain_text=u'Some Text', html_text=u'<b>Some Bold Text</b>', username=smtp.SMTP_USERNAME, password=smtp.SMTP_PASSWORD, require_starttls=True)
-        time.sleep(smtp.SMTP_RATE_LIMIT_SECONDS)
 
 
 if __name__ == u'__main__':
--- tests/test_sender.py
+++ tests/test_sender.py
@@ -6,19 +6,24 @@ import tests.smtp as smtp
 
 
 class TestSender(unittest.TestCase):
+    def run(self, *args, **kwargs):
+        result = super().run(*args, **kwargs)
+
+        time.sleep(smtp.SMTP_RATE_LIMIT_SECONDS)
+
+        return result
+
     def test_minimal(self):
         msg = builder.build_msg(u'Example <example@example.com>', u'The Subject',
                                 send_to=u'Test <test@test.com>', plain_text=u'Some Text', html_text=u'<b>Some Bold Text</b>')
         sender.send_msg(msg, smtp.SMTP_HOST, smtp.SMTP_PORT, username=smtp.SMTP_USERNAME,
                         password=smtp.SMTP_PASSWORD, require_starttls=True)
-        time.sleep(smtp.SMTP_RATE_LIMIT_SECONDS)
 
     def test_multiple_recipients(self):
         msg = builder.build_msg(u'Example <example@example.com>', u'The Subject', send_to=u'Test <test@test.com>', send_cc=[
                                 u'Example <example@example.com>', u'Example2 <example2@example.com>'], send_bcc=u'Example3 <example3@example.com>', plain_text=u'Some Text', html_text=u'<b>Some Bold Text</b>')
         sender.send_msg(msg, smtp.SMTP_HOST, smtp.SMTP_PORT, username=smtp.SMTP_USERNAME,
                         password=smtp.SMTP_PASSWORD, require_starttls=True)
-        time.sleep(smtp.SMTP_RATE_LIMIT_SECONDS)
 
 
 if __name__ == u'__main__':
